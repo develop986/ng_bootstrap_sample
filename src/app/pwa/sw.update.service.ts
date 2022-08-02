cc28e589a4d0793820ee19600688be4a865a6256
develop986 <97202707+develop986@users.noreply.github.com>
2022-08-02 14:48:38 +0900

    Update sw.update.service.ts
    SwUpdateService修正

--- src/app/pwa/sw.update.service.ts
+++ src/app/pwa/sw.update.service.ts
@@ -1,43 +1,53 @@
 import { Injectable, ApplicationRef } from "@angular/core";
-import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
+import { SwUpdate } from '@angular/service-worker';
 import { concat, interval } from 'rxjs';
 import { first } from 'rxjs/operators';
-
-function promptUser(event: UpdateAvailableEvent): boolean {
-    return true;
-}
 
 @Injectable()
 export class SwUpdateService {
 
     constructor(appRef: ApplicationRef, updates: SwUpdate) {
         console.log('SwUpdate isEnabled:', updates.isEnabled);
+        console.log('バージョン', 7);
+
+        updates.versionUpdates.subscribe(evt => {
+            console.log(`versionUpdates`, evt);
+            switch (evt.type) {
+                case 'VERSION_DETECTED':
+                    console.log(`Downloading new app version: ${evt.version.hash}`);
+                    break;
+                case 'VERSION_READY':
+                    console.log(`Current app version: ${evt.currentVersion.hash}`);
+                    console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+                    updates.activateUpdate().then(() => {
+                        console.log('activateUpdate End');
+                        document.location.reload()
+                    });
+                    break;
+                case 'VERSION_INSTALLATION_FAILED':
+                    console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+                    break;
+                case 'NO_NEW_VERSION_DETECTED':
+                    console.log(`NO_NEW_VERSION_DETECTED '${evt.version.hash}'`);
+                    break;
+            }
+        });
+
         if (updates.isEnabled) {
-            updates.available.subscribe(event => {
-                console.log('current version is', event.current);
-                console.log('available version is', event.available);
-            });
-            updates.activated.subscribe(event => {
-                console.log('old version was', event.previous);
-                console.log('new version is', event.current);
-            });
-            
-            // Allow the app to stabilize first, before starting polling for updates with `interval()`.
+
+            // Allow the app to stabilize first, before starting
+            // polling for updates with `interval()`.
             const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
-            const everySixHours$ = interval(6 * 60 * 60 * 1000);
+            //const everySixHours$ = interval(6 * 60 * 60 * 1000);
+            const everySixHours$ = interval(6 * 60 * 60);
             const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
 
             everySixHoursOnceAppIsStable$.subscribe(() => {
                 console.log('checkForUpdate');
-                updates.checkForUpdate()
+                updates.checkForUpdate().then(() => {
+                    console.log('checkForUpdate End');
+                });
             });
-
-            updates.available.subscribe(event => {
-                console.log("promptUser:", promptUser(event));
-                if (promptUser(event)) {
-                    updates.activateUpdate().then(() => document.location.reload());
-                }
-            });
-        }
+        };
     }
 }
